3b4ec25117f10ac81611e4cf3aa9aa2637c0530e
AnjanaTulasi <anjanatdeverapalli@gmail.com>
2025-03-07 17:45:03 -0600

    new pages added

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,5 @@
 function scrollToSection(sectionId) {
     document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> dabc6333e8912093ba967060b0f66a0a61d64838
+
+
